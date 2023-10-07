8bd2bf937cb44d9880fd6ca596bd9801a5f132dd
Akib Saleh <akibzihan@gmail.com>
2023-10-07 14:25:54 +0600

    Tailwind custom class configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        archivo: ['Archivo', 'sans-serif'],
+        nsb: ['Noto Serif Bengali', 'serif'],
+      },
+      colors: {
+        plant: {
+          500: '#87C6B6',
+          700: '#215A4D',
+          800: '#19493E',
+        },
+        apache: '#ECB869',
+        thunder: {
+          500: '#585A6A',
+          700: '#272836',
+        },
+        haze: '#F2F3F8',
+        snow: '#F9FAFC',
+      },
+    },
   },
   plugins: [],
 };
