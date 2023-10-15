a096c6cb17a017c61c8a93f874eb619069b31cfd
Tanvir Rudro <rudro987@gmail.com>
2023-10-15 23:21:22 +0600

    application form semi completed

--- src/Components/Application/Application.jsx
+++ src/Components/Application/Application.jsx
@@ -5,9 +5,9 @@ const Application = () => {
   const { t, i18n } = useTranslation('global');
   const isBn = i18n.language === 'bn';
   return (
-    <div className='max-w-[794px] mx-auto pt-[140px]'>
+    <div className='max-w-[794px] mx-auto mt-[140px]'>
       <h1 className='text-center text-[40px] mb-3'><span className={isBn && 'font-lss'}>{t('application.title')}</span></h1>
-      <p className='text-center text-[22px]'>{t('application.description')}</p>
+      <p className='text-center text-[22px] px-20 mb-12'>{(t('application.description')).split('\n\n')}</p>
       <ApplicationForm />
     </div>
   );
--- src/Components/Application/ApplicationForm.jsx
+++ src/Components/Application/ApplicationForm.jsx
@@ -1,8 +1,33 @@
+import { useTranslation } from "react-i18next";
+
 const ApplicationForm = () => {
+  const { t, i18n } = useTranslation("global");
+  const isBn = i18n.language === "bn";
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div>
-      <h1>Application form by rudro987</h1>
-      <h2>creating pull request</h2>
+      <form onSubmit={handleSubmit}>
+      <div className="flex gap-6">
+        <div className="w-[210px] font-medium text-lg text-thunder-500">
+          <label>
+            <span>{t("applicationForm.nameLabel")}</span>
+            <span className="text-[#F02727]">*</span>
+          </label>
+        </div>
+        <div>
+          <input
+            name="name"
+            type="text"
+            placeholder={t("applicationForm.namePlaceHolder")}
+            required
+          />
+        </div>
+        </div>
+      </form>
     </div>
   );
 };
