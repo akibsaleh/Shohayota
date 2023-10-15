893821dac8c55949b31a27d2a2551c7752a7e11f
Akib Saleh <akibzihan@gmail.com>
2023-10-15 19:22:51 +0600

    Achievement component complete

--- src/Components/Achievements/Achievements.jsx
+++ src/Components/Achievements/Achievements.jsx
@@ -1,4 +1,6 @@
 import { useTranslation } from 'react-i18next';
+import people from '../../assets/people-fill.svg';
+import gift from '../../assets/gift-fill.svg';
 const Achievements = () => {
   const { t, i18n } = useTranslation('global');
   const isBn = i18n.language === 'bn';
@@ -6,7 +8,7 @@ const Achievements = () => {
 
   return (
     <div className="relative max-w-screen-xl mx-auto bg-apache min-h-[212px] -mt-[106px] z-20 rounded-[20px] px-[60px] py-[50px] flex">
-      <div>
+      <div className="w-6/12">
         <h2 className="text-[42px] text-thunder-700 leading-snug font-bold">
           {isBn && (
             <span className="font-lss">
@@ -23,6 +25,34 @@ const Achievements = () => {
             </span>
           )}
         </h2>
+      </div>
+      <div className="w-6/12 flex items-center">
+        <div className="w-1/2 flex items-center justify-center gap-x-6 text-thunder-700">
+          <img
+            src={people}
+            alt="Assited"
+            className="w-[68] h-auto p-[14px] bg-white/40 rounded-[20px]"
+          />
+          <div>
+            <p className="text-2xl font-medium leading-[36px] -mb-[5px]">{t('achieve.assist')}</p>
+            <p className="text-[26px] font-bold leading-[48px]">
+              {t('achieve.assist_no')} {isBn ? 'জন' : 'Individuals'}
+            </p>
+          </div>
+        </div>
+        <div className="w-1/2 flex justify-center items-center gap-x-6 text-thunder-700">
+          <img
+            src={gift}
+            alt="Donated"
+            className="w-[68] h-auto p-[14px] bg-white/40 rounded-[20px]"
+          />
+          <div>
+            <p className="text-2xl font-medium leading-[36px] -mb-[5px]">{t('achieve.donate')}</p>
+            <p className="text-[26px] font-bold leading-[48px]">
+              {t('achieve.donated_amount')} {isBn ? 'টাকা' : 'BDT'}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
