b8e6e5a28e478e1e2714073cfbd15988c0a7e62c
Akib Saleh <akibzihan@gmail.com>
2023-10-15 15:48:43 +0600

    Banner Layout Complete

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -11,15 +11,19 @@ const Banner = () => {
           {isBn && <span className="font-lss">পরিচয় গোপন রেখে ক্ষুদ্র আর্থিক সহায়তা</span>}
           {isEng && <span className="font-archivo">A Small, Anonymous Financial Assistance</span>}
         </h1>
-        <p className="text-plant-100 text-sxl max-w-3xl text-center">
+        <p className="text-plant-100 text-sxl max-w-[830px] text-center">
           {isBn && <span className="font-nsb">আমাদের সীমিত আর্থিক সহায়তা যদি আপনার বা আপনার পরিবারের প্রশস্তি দিতে সক্ষম হয়, আমরা সর্বত্তম চেষ্টা করবো আপনার বা আপনার পরিবারের পাশে দাঁড়াতে।</span>}
           {isEng && (
-            <span className="font-archivo">
+            <span className="font-archivo font-light">
               We believe in the power of small acts of kindness. We provide discreet financial assistance to those in need, ensuring your identity remains confidential. Together, we make a difference,
               one small helping hand at a time.
             </span>
           )}
         </p>
+        <button className="bg-white text-2xl leading-snug font-semibold py-3 px-8 rounded-full mt-12">
+          {isBn && <span className="font-nsb">{t('banner.button')}</span>}
+          {isEng && <span className="font-archivo">{t('banner.button')}</span>}
+        </button>
       </div>
     </div>
   );
