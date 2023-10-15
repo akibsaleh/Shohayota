6b9046e4d1d6efefd8afa94e7d53a2e5f23bfe72
Akib Saleh <akibzihan@gmail.com>
2023-10-15 18:48:10 +0600

    achievement partially developed

--- src/Components/Achievements/Achievements.jsx
+++ src/Components/Achievements/Achievements.jsx
@@ -1,11 +1,29 @@
+import { useTranslation } from 'react-i18next';
 const Achievements = () => {
+  const { t, i18n } = useTranslation('global');
+  const isBn = i18n.language === 'bn';
+  const isEng = i18n.language === 'en';
+
   return (
-    <div>
-      <h1>Achievements by akibzihan</h1>
-      <p>This is a change</p>
-      <p>This is a change by rudro987</p>
-      <p>I am zihan</p>
-      <p>done by rudro987</p>
+    <div className="relative max-w-screen-xl mx-auto bg-apache min-h-[212px] -mt-[106px] z-20 rounded-[20px] px-[60px] py-[50px] flex">
+      <div>
+        <h2 className="text-[42px] text-thunder-700 leading-snug font-bold">
+          {isBn && (
+            <span className="font-lss">
+              এখন পর্যন্ত
+              <br />
+              আমাদের অর্জনসমূহ
+            </span>
+          )}
+          {isEng && (
+            <span className="font-archivo">
+              Our Accomplishments
+              <br />
+              to Date
+            </span>
+          )}
+        </h2>
+      </div>
     </div>
   );
 };
