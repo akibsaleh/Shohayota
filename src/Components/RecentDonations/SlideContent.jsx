8702b846c4feb5ace1343fbbba7e323f1934665f
Akib Saleh <akibzihan@gmail.com>
2023-11-16 14:32:04 +0600

    Meta tgas fix

--- src/Components/RecentDonations/SlideContent.jsx
+++ src/Components/RecentDonations/SlideContent.jsx
@@ -8,17 +8,16 @@ const SlideContent = ({ item }) => {
   const { t, i18n } = useTranslation('global');
   const isBn = i18n.language === 'bn';
   const isEn = i18n.language === 'en';
-  console.log(item);
 
   return (
     <div className="flex flex-col items-center">
-      <p className={`${isBn ? 'font-lss' : 'font-archivo'} text-thunder-500`}>{isBn ? item?.approveBanglaDate : isEn ? item?.approveDate : undefined}</p>
+      <p className={`${isBn ? 'font-lss' : 'font-archivo'} text-thunder-700/60 [.swiper-slide-active_&]:text-thunder-700`}>{isBn ? item?.approveBanglaDate : isEn ? item?.approveDate : undefined}</p>
       <div className="pt-5 pb-14 relative">
         <PiCircleFill className="absolute [.swiper-slide-active_&]:text-plant-700 stroke-snow text-snow top-[21px] left-1/2 -translate-x-1/2 stroke-[20px] text-sm" />
         <BsCircleFill className="border-[2px] border-plant-700 -mt-[1px] text-snow rounded-full text-lg" />
       </div>
 
-      <div className="slideContentContainer w-ful flex flex-col justify-center items-center">
+      <div className="slideContentContainer w-ful flex flex-col justify-center items-center text-thunder-700/60 [.swiper-slide-active_&]:text-thunder-700">
         <div className="relative duration-200 transition-all slideContent w-[276px] h-[115px] px-3 py-5 bg-white shadow-donateBox rounded-md text-center drop-shadow-md flex flex-col justify-center items-center">
           <h3 className="flex justify-center items-center -ml-3 leading-[22px]">
             <PiMapPinDuotone className="mr-2" />
